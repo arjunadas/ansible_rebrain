b7e290a89f148fba2dc30efa027cfe45fc44df91
aleksey.yakurnov <aleksey.yakurnov@bpmsoft.ru>
2025-05-22 09:20:20 +0300

    add new files

---
+++ pract11/to_do.ps1
@@ -0,0 +1,24 @@
+#Создайте волт /home/user/task/nginx.vault и паролем vaultSecret@!
+
+cd /home/user/task
+
+pass="vaultSecret@!"
+echo $pass > /home/user/task/.vault_pass.txt
+
+
+cat << EOF > /home/user/task/nginx.vault
+---
+nginx_user : user
+nginx_pass : secretPassword123!
+EOF
+
+
+#Шифруем файл vars/exmple_text.yml:
+
+ansible-vault encrypt /home/user/task/nginx.vault --vault-password-file .vault_pass.txt
+
+#запускаем плейбук:
+
+ansible-playbook -i inventory.yaml site.yaml -e "@/home/user/task/nginx.vault"  --vault-password-file .vault_pass.txt
+
+
